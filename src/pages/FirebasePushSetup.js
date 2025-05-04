c0e3ceca2acd258e09c1ce2b5ed8f99038c831f0
Yachadajean <97514042+Yachadajean@users.noreply.github.com>
2025-05-04 14:32:41 +0800

    Update FirebasePushSetup.js

--- src/pages/FirebasePushSetup.js
+++ src/pages/FirebasePushSetup.js
@@ -11,7 +11,7 @@ const FirebasePushSetup = () => {
           const token = await getToken(messaging, {
             vapidKey: 'BKDr9lAqH-s2eMkTkUJMis7LLxZWqHkXNveZrPM3wg6N400X9Pt4iCw2Bx1lWTcSPN4a7O8MASH8P0FQ2H4CNjk', // Replace with your Firebase VAPID key
           });
-          console.log('📱 Firebase device token:', token);
+          localStorage.setItem('fcmToken', token);
           // TODO: send `token` to your backend or store locally
         } else {
           console.warn('🔕 Notification permission denied');
